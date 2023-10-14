43dd20d3da922c239ac2270480085c9be207bd30
Hroi Sigurdsson <hroi@asdf.dk>
2023-10-14 20:54:25 +0200

    add convert-all-sh

---
+++ convert-all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -euo pipefail
+
+KITTY_THEMES_DIR=../kitty-themes/themes
+
+if [ ! -d "$KITTY_THEMES_DIR" ]; then
+  printf "missing themes dir in %s\n" "$KITTY_THEMES_DIR"
+  exit 1
+fi
+
+for f in "$KITTY_THEMES_DIR"/*.conf
+do
+  themename=$(basename "${f/.conf}")
+  ./convert-from-kitty.gawk < "$f" > ./themes/"$themename"
+done
